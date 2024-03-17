Fix tomography for qubit case. Adjusted commutation calculation to not overcount.

src/sdim/unitary.py
@@ -34,9 +34,12 @@ def generate_p_matrix(d):
     return P
 
 def generate_cnot_matrix(d):
-    CNOT = np.zeros((d**2, d**2))
+    omega = np.exp(2j * np.pi / d)
+    CNOT = np.zeros((d**2, d**2),dtype=np.complex128)
     for i, j in product(range(d), repeat=2):
-        CNOT[d * i + j, d * i + (i + j) % d] = 1
+        # phase = omega**((i + j) / 2) if d % 2 == 0 else 1
+        phase = 1
+        CNOT[d * i + j, d * i + (i + j) % d] = phase
     CNOT = CNOT.reshape(d**2, d**2)
     CNOT = CNOT.transpose()
     return CNOT

tests/statevector/test_tomography.py
@@ -1,19 +1,19 @@
 from sdim.program import Program
 from sdim.chp_parser import read_circuit
-from tomography import cirq_statevector_from_circuit
+from tomography import cirq_statevector_from_circuit, numpy_statevector_from_circuit
 import numpy as np
 import itertools
-def test_rand_circuit(num_samples=100000, print_prob=False):
+def test_rand_circuit(num_samples=10000, print_prob=False):
     circuit = read_circuit("profiling/random_circuit.chp")
     statevector = cirq_statevector_from_circuit(circuit)
     amplitudes = np.abs(statevector)**2
 
     # Get the number of qudits and the dimension from the circuit.
     n = circuit.num_qudits
-    D = [str(i) for i in range(circuit.dimension)]
+    basis_states = [str(i) for i in range(circuit.dimension)]
 
     # Generate all possible keys.
-    all_possible_keys = [''.join(key) for key in itertools.product(D, repeat=n)]
+    all_possible_keys = [''.join(key) for key in itertools.product(basis_states, repeat=n)]
 
     # Initialize the dictionary with all possible keys.
     measurement_results = {key: 0 for key in all_possible_keys}
@@ -23,9 +23,9 @@ def test_rand_circuit(num_samples=100000, print_prob=False):
         program = Program(circuit)
         program.simulate()
         measurements = program.measurement_results
-
         # Construct a key for each sample.
         key = ''.join(str(result) for _, _, result in measurements)
+
 
         # Increment the count for this key.
         if key not in measurement_results:
@@ -47,9 +47,7 @@ def test_rand_circuit(num_samples=100000, print_prob=False):
 prob, amp = test_rand_circuit()
 threshold = 1e-14
 cleaned_amp = np.where(np.abs(amp) < threshold, 0, amp)
-
-print(prob)
-print(cleaned_amp)
-
-# close = np.allclose(prob, amp, atol=1e-3)
-# print(close)
+tvd = np.sum(np.abs(prob - cleaned_amp)) / 2
+print(f"Total Variation Distance: {tvd}")
+# print(f"Amplitudes: {cleaned_amp}")
+# print(f"Probabilities: {prob}")

tests/statevector/tomography.py
@@ -39,6 +39,50 @@ def cirq_statevector_from_circuit(circuit):
     # Simulate the Cirq circuit.
     simulator = cirq.Simulator()
     result = simulator.simulate(cirq_circuit, initial_state=state)
-
+    # print(cirq_circuit)
     # Return the final state vector.
     return result.final_state_vector
+
+def numpy_statevector_from_circuit(circuit):
+    # Start with an initial state. For a quantum computer, this is usually the state |0...0>.
+    state = np.zeros((circuit.dimension**circuit.num_qudits,), dtype=np.complex128)
+    state[0] = 1
+
+    # Create the generalized gates.
+    gate_map = {
+        "H": GeneralizedHadamardGate(circuit.dimension),
+        "P": GeneralizedPhaseShiftGate(circuit.dimension),
+        "CNOT": GeneralizedCNOTGate(circuit.dimension),
+        "M": IdentityGate(circuit.dimension)
+    }
+
+    # Apply each gate in the circuit.
+    for op in circuit.operations:
+        if op.name in gate_map:
+            gate = gate_map[op.name]
+        else:
+            raise ValueError(f"Gate {op.name} not found")
+
+        # Create a list of identities with the actual gate operation at the correct qudit index.
+        gate_list = [np.eye(circuit.dimension, dtype=np.complex128) for _ in range(circuit.num_qudits)]
+        if op.target_index is None:
+            # Single-qudit gate.
+            gate_list[op.qudit_index] = gate
+        else:
+            # Two-qudit gate.
+            control_index = min(op.qudit_index, op.target_index)
+            target_index = max(op.qudit_index, op.target_index)
+            gate_list[control_index] = gate
+            gate_list.pop(target_index)
+
+        # Combine the gate list elements using Kronecker products.
+        gate_matrix = np.eye(1, dtype=np.complex128)
+        for gate in gate_list:
+            gate_matrix = np.kron(gate_matrix, gate)
+
+        # Apply the gate to the state.
+        print(gate_matrix)
+        state = np.dot(gate_matrix, state)
+
+    # Construct full size unitary matrix
+    return state